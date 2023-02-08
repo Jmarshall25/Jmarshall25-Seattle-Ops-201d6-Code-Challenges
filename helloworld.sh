56cdf192455d3002a40a4aab152d82725cbc636e
jmarshall25 <jordanmarshall25@yahoo.com>
2023-02-08 01:14:19 +0000

    how am i

---
+++ helloworld.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: Jordan Marshall
+# Date of latest revision: 7Feb23
+# Purpose: Print a string to the terminal
+
+
+
+#Main
+
+
+echo "I am working perfectly"
+
+
+#End
