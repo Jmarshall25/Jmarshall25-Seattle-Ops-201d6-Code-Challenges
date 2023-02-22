ee70319894fad0db8a154906cb132795efdb57ea
Jordan Marshall <121899029+Jmarshall25@users.noreply.github.com>
2023-02-22 01:34:29 -0500

    Update Code-Challenge-10.ps1

--- Code-Challenge-10.ps1
+++ Code-Challenge-10.ps1
@@ -1,14 +1,14 @@
-# Get-Process | Sort-Object -Descending CPU | Format-Table -AutoSize Name, CPU
+ Get-Process | Sort-Object -Descending CPU | Format-Table -AutoSize Name, CPU
 
-# Get-Process | Sort-Object -Descending Id | Format-Table -AutoSize Name, Id
+Get-Process | Sort-Object -Descending Id | Format-Table -AutoSize Name, Id
 
-# Get-Process | Sort-Object -Descending WorkingSet | Select-Object -First 5 Name, WorkingSet | Format-Table -AutoSize
+Get-Process | Sort-Object -Descending WorkingSet | Select-Object -First 5 Name, WorkingSet | Format-Table -AutoSize
 
-# Start-Process iexplore.exe "https://owasp.org/www-project-top-ten/"
+Start-Process iexplore.exe "https://owasp.org/www-project-top-ten/"
 
-# for ($i = 1; $i -le 10; $i++) {
-#    Start-Process iexplore.exe "https://owasp.org/www-project-top-ten/"
-#    Start-Sleep -Seconds 2
-# } 
+ for ($i = 1; $i -le 10; $i++) {
+    Start-Process iexplore.exe "https://owasp.org/www-project-top-ten/"
+    Start-Sleep -Seconds 2
+ } 
 
-# Get-Process "iexplore" | Stop-Process -Force 
+ Get-Process "iexplore" | Stop-Process -Force 
