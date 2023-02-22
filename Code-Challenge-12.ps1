6181b9d2e3da37b16007e9737113f6874b0b4c85
Jordan Marshall <121899029+Jmarshall25@users.noreply.github.com>
2023-02-22 01:33:09 -0500

    Update Code-Challenge-12.ps1

--- Code-Challenge-12.ps1
+++ Code-Challenge-12.ps1
@@ -2,6 +2,7 @@
 #Use Select-String to search network_report.txt and return only the IP version 4 address.
 #Remove the network_report.txt when you are finished searching it.
 #For this challenge, you must use at least one variable and one function.
+#Used https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/select-string?view=powershell-7.3&viewFallbackFrom=powershell-7.1 for reference
 
 
 $ip_output = ipconfig /all > "network_report.txt"
