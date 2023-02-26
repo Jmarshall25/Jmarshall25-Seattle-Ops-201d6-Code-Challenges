af306acc44c992ba8e4e6b71d1d952777662d77e
Jordan Marshall <121899029+Jmarshall25@users.noreply.github.com>
2023-02-26 17:57:56 -0500

    Update Code-Challenge-05.sh

--- Code-Challenge-05.sh
+++ Code-Challenge-05.sh
@@ -1,1 +1,22 @@
-unable to complete yet
+#!/bin/bash
+
+# Script: Ops 201 Class 05 Ops Challenge Solution
+# Author: Jordan Marshall
+# Date of latest revision: 25Feb23
+# Purpose: Loops
+
+
+# Main
+
+while true; do
+    # Running Processes
+    echo "List of running processes:"
+    ps aux
+
+    # Ask the user for a PID to kill
+    read -p "Enter the PID of the process you want to kill (or q to quit): " input
+
+    # Kill the process with the given PID
+    kill "$input"
+
+# End
